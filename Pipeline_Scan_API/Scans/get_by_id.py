c197b7a460db8d1b5fe0d5b16eba514b9092e8e0
A.Fernando Oliveira <fernando.oliveira@qriar.com>
2023-03-29 17:21:41 -0300

    Updating Pipeline Scan Usage Samples

---
+++ Pipeline_Scan_API/Scans/get_by_id.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+import pprint
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+
+
+api_base = "https://api.veracode.com"
+headers = {"User-Agent": "Python HMAC Example"}
+
+scan_id = "scan_id"
+
+if __name__ == "__main__":
+
+    try:
+        response = requests.get(api_base + "/api/authn/v2/pipeline_scan/scans/"+scan_id, auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+    except requests.RequestException as e:
+        print("Whoops!")
+        print(e)
+        sys.exit(1)
+
+    if response.ok:
+        data = response.json()
+        pprint.pprint(data)
+        
+    else:
+        print(response.status_code)
