b9c7541d8fb6d7b3765179c08af3db2bc3f2600d
A.Fernando Oliveira <fernando.oliveira@qriar.com>
2023-04-05 14:57:31 -0300

    Adionando exemplos para SCA Agent Scan

---
+++ SCA_Agent_API/scans/get_workspace.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+import pprint
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+
+
+api_base = "https://api.veracode.com/srcclr"
+headers = {"User-Agent": "Python HMAC Example"}
+
+scanID = "scanID"
+
+if __name__ == "__main__":
+
+    try:
+        response = requests.get(api_base + "/v3/scans/"+scanID, auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+    except requests.RequestException as e:
+        print("Whoops!")
+        print(e)
+        sys.exit(1)
+
+    if response.ok:
+        data = response.json()
+        pprint.pprint(data)
+        
+    else:
+        print(response.status_code)
